2dbacee9fd3cbdbc25439b95ee878a6a52c9bceb
Moses Mok <mosesmokmomo@gmail.com>
2026-06-19 19:47:30 +0100

    Main page
    (From Claude)

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const fs = require("fs");
 const router = express.Router();
 
-router.get(["/", "/index.html"], (req, res) => {
-    console.log(`${req.method} ${req.url}`);
-    res.send(`<p>This is an Express site, running on Node.js ${process.version}.</p>
-              <p>Links: <a href="${req.baseUrl}${req.url}">Self</a>,
-                        <a href="${req.baseUrl}/">Root</a>,
-                        <a href="${req.baseUrl}/page">Sub-page</a></p>`);
-});
-
+// router.get(["/", "/index.html"], (req, res) => {
+//     res.sendFile("./public/home.html", { root: __dirname });
+//     // console.log(`${req.method} ${req.url}`);
+//     // res.send(`<p>This is an Express site, running on Node.js ${process.version}.</p>
+//     //           <p>Links: <a href="${req.baseUrl}${req.url}">Self</a>,
+//     //                     <a href="${req.baseUrl}/">Root</a>,
+//     //                     <a href="${req.baseUrl}/page">Sub-page</a></p>`);
+// });
+//
 const socket = process.argv[2] || 8000;
 const mount = process.argv[3] || "/";
 
@@ -22,6 +23,7 @@ if (fs.existsSync(socket)) fs.unlinkSync(socket);
 
 const app = express();
 app.use(mount, router);
+app.use(express.static("public"));
 
 const server = app.listen(socket, () => console.log(`Ready for requests (socket: ${socket}, mount: ${mount})`));
 
